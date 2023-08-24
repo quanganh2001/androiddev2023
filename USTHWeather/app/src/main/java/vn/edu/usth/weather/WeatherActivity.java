913bdb92110243c1216e28a1c1f7dd7a7e3defa9
quanganh2001 <cocoi5659@gmail.com>
2023-08-24 20:49:26 +0700

    Practical Work 3: Create Fragment, set background color and add the Fragment to the 'container' FrameLayout

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -13,6 +13,11 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        // Create a new Fragment to be placed in the activity layout
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
         Log.i(TAG, "=== APP CREATED ===");
     }
 
