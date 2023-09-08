286fefdfe522e6d13fcfbad18508733368cbe0a0
quanganh2001 <cocoi5659@gmail.com>
2023-09-08 16:36:07 +0700

    Practical Work 5: Modify your ForecastFragment layout and Use LinearLayout to have the blue forecast area

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WeatherFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WeatherFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
